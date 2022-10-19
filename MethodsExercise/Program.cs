6c3ab2783801c07857d421f589c9db80aeee6e6e
Colton Christopher <2tontide@gmail.com>
2022-10-19 10:52:02 -0500

    began user question

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace MethodsExercise
+﻿using Internal;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Hi {userName}. What is your favorite color?")
+                var color = Console.ReadLine();
+
+            Console.WriteLine("{color} is an awsome color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! Now what is your favorite band?");
+            var band = Console.ReadLine();
+
         }
     }
 }

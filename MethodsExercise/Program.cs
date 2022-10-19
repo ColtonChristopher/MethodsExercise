0306a6b2addcef08245fea1103a9892078500166
Colton Christopher <2tontide@gmail.com>
2022-10-19 11:38:59 -0500

    all excercises done

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,8 +4,40 @@ namespace MethodsExercise
 {
     public class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2
+        }
+
+        public static int Sub(int num1, int num2)
+        {
+            return num1 - num2
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1/num2
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2 
+        }
+
         static void Main(string[] args)
         {
+
+            var amountOfCars = Sum(3, 7);
+            var amountOfGroups = Multiply(5, 7)
+            var amountOfCakes = Sub(10, 3)
+            var amountOfSomething = Divide(9, 3)
+            var amountOfSomething = Modulus(100, 9)
+
             Console.WriteLine("What is your name?");
             string userName = Console.ReadLine();
 
@@ -17,6 +49,8 @@ namespace MethodsExercise
 
             Console.WriteLine("Great! Now what is your favorite band?");
             var band = Console.ReadLine();
+
+           Console.WriteLine($"Hello my name is {userName}, I always thought it would be funny to see my favorite animal a {animal} as my favorite color {color} walking along on two legs listening to my favorite band {band}.")
 
         }
     }
